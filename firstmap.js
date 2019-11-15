3b32befaef735ee2d35eee0a6c5aa48611ba65c3
Hasani Valdez <54682559+Hvaldez2@users.noreply.github.com>
2019-11-14 21:24:37 -0600

    adding markers to my map

--- firstmap.js
+++ firstmap.js
@@ -1,11 +1,86 @@
 var map;
 
-function initMap(){
+function initMap() {
 
-	map = new google.maps.Map(document.getElementById("map"), {
-          center: {lat: 41.868446, lng: -87.629557},
-          zoom: 8
-        });
-		
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: {
+      lat: 44.609701,
+      lng: 12.840961
+    },
+    zoom: 3
+  });
+  //paris
+  var contentString = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">Paris</h1>'+
+            '<div id="bodyContent">'+
+            '<p>Paris is the true capital of the arts and sciences. I was lucky to visit in 2012 and in 2015.</p>'+
+            '</div>'+
+            '</div>';
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+  var marker = new google.maps.Marker({
+    position: {
+      lat: 48.8566,
+      lng: 2.3522
+    },
+    map: map,
+    title: 'Paris'
+  });
+  marker.addListener('click', function () {
+    infowindow.open(map, marker);
+  });
+  
+  //barcelona
+  var contentStringBarcelona = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">Barcelona</h1>'+
+            '<div id="bodyContent">'+
+            '<p>On a layover, I got to visit the beautiful beaches and visit the still incomplete Sagrada Familia .</p>'+
+            '</div>'+
+            '</div>';
+  var infowindowBarcelona = new google.maps.InfoWindow({
+    content: contentStringBarcelona
+  });
+  var markerBarcelona = new google.maps.Marker({
+    position: {
+      lat: 41.3851,
+      lng: 2.1734
+    },
+    map: map,
+    title: 'Barcelona'
+  });
+  markerBarcelona.addListener('click', function () {
+    infowindowBarcelona.open(map, markerBarcelona);
+  });
+  
+  //Istanbul
+  
+  var istanbulString = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">Istanbul</h1>'+
+            '<div id="bodyContent">'+
+            '<p>By far the best city I have ever visited. It is so cosmopolitan and I met amazing Turkish soldiers who showed me around</p>'+
+            '</div>'+
+            '</div>';
+  var istanbulInfo = new google.maps.InfoWindow({
+    content: istanbulString
+  });
+  var markerIstanbul = new google.maps.Marker({
+    position: {
+      lat: 41.0082,
+      lng: 28.9784
+    },
+    map: map,
+    title: 'Istanbul'
+  });
+  markerIstanbul.addListener('click', function () {
+    istanbulInfo.open(map, markerIstanbul);
+  });
+
 }
 google.maps.event.addDomListener(window, 'load', init);
