0a18f906776ae587dad6776f2b22f072ea787a9e
Hasani Valdez <54682559+Hvaldez2@users.noreply.github.com>
2019-11-14 21:43:15 -0600

    designing CSS for javascript map and adding JAVSCRIPT MAP CODED

--- firstmap.js
+++ firstmap.js
@@ -4,8 +4,9 @@ function initMap() {
 
   map = new google.maps.Map(document.getElementById("map"), {
     center: {
-      lat: 44.609701,
-      lng: 12.840961
+      lat: 12.896177,
+      lng: 38.687014
+
     },
     zoom: 3
   });
@@ -81,6 +82,128 @@ function initMap() {
   markerIstanbul.addListener('click', function () {
     istanbulInfo.open(map, markerIstanbul);
   });
+  
+  //siem riep
+  
+  var siemreap = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">Siem Reap</h1>'+
+            '<div id="bodyContent">'+
+			'<img class="indeximage" src="./images/cambodia.jpg" title="My Cambodia Vacation" alt="Cambodia" />'+
+            '<p>My most recent adventure. Lots to love about this city! Great culture, awesome ancient temples of Angkor Wat, tasty food!</p>'+
+            '</div>'+
+            '</div>';
+  var siemreapinfo = new google.maps.InfoWindow({
+    content: siemreap
+  });
+  var markerSiem = new google.maps.Marker({
+    position: {
+      lat: 13.3573,
+      lng: 103.8540
+    },
+    map: map,
+    title: 'Siem Reap'
+  });
+  markerSiem.addListener('click', function () {
+    siemreapinfo.open(map, markerSiem);
+  });
+  
+  //south Africa
+  var joburg = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">South Africa</h1>'+
+            '<div id="bodyContent">'+
+            '<p>I got to meet lions and go on a safari! Awesome</p>'+
+            '</div>'+
+            '</div>';
+  var joburgInfo = new google.maps.InfoWindow({
+    content: joburg
+  });
+  var markerJoburg = new google.maps.Marker({
+    position: {
+      lat: -26.2041,
+      lng: 28.0473
+    },
+    map: map,
+    title: 'Johanesburg'
+  });
+  markerJoburg.addListener('click', function () {
+    joburgInfo.open(map, markerJoburg);
+  });
+  
+  //Bangkok
+  
+  var bangkok = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">Bangkok</h1>'+
+            '<div id="bodyContent">'+
+            '<p>Bangkok was a lot of fun! The red light district was amazing  I highly recommend for all college students to attend</p>'+
+            '</div>'+
+            '</div>';
+  var bangkokInfo = new google.maps.InfoWindow({
+    content: bangkok
+  });
+  var markerBangkok = new google.maps.Marker({
+    position: {
+      lat: 13.7563,
+      lng:100.5018
+    },
+    map: map,
+    title: 'Bangkok'
+  });
+  markerBangkok.addListener('click', function () {
+    bangkokInfo.open(map, markerBangkok);
+  });
+  
+  //greece
+  var greece = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">Lefkada</h1>'+
+            '<div id="bodyContent">'+
+            '<p>This was the craziest place Ive been too, id rather not talk about it....</p>'+
+            '</div>'+
+            '</div>';
+  var greekInfo = new google.maps.InfoWindow({
+    content: greece
+  });
+  var markerGreece = new google.maps.Marker({
+    position: {
+      lat: 38.7066,
+      lng:20.6407
+    },
+    map: map,
+    title: 'Lefkada'
+  });
+  markerGreece.addListener('click', function () {
+    greekInfo.open(map, markerGreece);
+  });
+  
+  var flightPlanCoordinates = [
+          {lat: 48.8566,
+      lng: 2.3522},
+          {lat: 41.3851,
+      lng: 2.1734},
+          {lat: 38.7066,
+      lng:20.6407},{lat: 41.0082,
+      lng: 28.9784},
+	  {lat: -26.2041,
+      lng: 28.0473},{lat: 13.7563,
+      lng:100.5018},{lat: 13.3573,
+      lng: 103.8540}
+        ];
+        var flightPath = new google.maps.Polyline({
+          path: flightPlanCoordinates,
+          geodesic: true,
+          strokeColor: '#FF0000',
+          strokeOpacity: 1.0,
+          strokeWeight: 2
+        });
+
+        flightPath.setMap(map);
 
 }
 google.maps.event.addDomListener(window, 'load', init);
